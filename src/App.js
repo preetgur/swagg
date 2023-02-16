a914d1dcc6293512fe40488295f6cb869876092b
Rupinderkaushal <rupinder.xpertiks@gmail.com>
2023-02-16 18:46:12 +0530

    swaggerui setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = "http://localhost:3000/swagger-docs";
+  const live = "https://53nsznx3bl.execute-api.us-east-1.amazonaws.com/swagger-docs";
+  return <SwaggerUI url={live} />
 }
 
 export default App;
