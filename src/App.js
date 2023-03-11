b980b584d3ea2ee4732820d6e99a559ec47e082b
preetgur <preetgur0137@gmail.com>
2023-03-11 17:25:45 +0530

    create webrtc branch

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 
 function App() {
-  const url = "http://localhost:3000/swagger-docs";
-  const live = "https://53nsznx3bl.execute-api.us-east-1.amazonaws.com/swagger-docs";
-  return <SwaggerUI url={live} />
+  const local = "http://localhost:8000/swagger-docs";
+  return <SwaggerUI url={local} />
 }
 
 export default App;
